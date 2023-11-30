f2d0b1a92939c2f09bd87c1e38eb7c65ffd0a213
FREDY QUEZADA <63796300+3200302003@users.noreply.github.com>
2023-11-30 11:59:51 -0600

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/lugares', require('./lugaresAtencion'));
-app.use('/horarios', require('./horarios'));
-app.use('/detalleAgenda', require('./detalleAgenda'));
-app.use('/auth', require('./auth'));
-
-module.exports = app;
