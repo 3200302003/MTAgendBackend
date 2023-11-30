d9242effb2863635e9652856fed89ccdf9f2e7e4
erickalfredoqh <181428@utags.edu.mx>
2023-11-30 11:32:53 -0600

    Servidor de Backend de MTAgend

---
+++ index.js
@@ -0,0 +1,39 @@
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Habilitación de CORS
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const agendaApp = require('./routes/index');
+
+// Conexión a MongoDB
+mongoose.connect(process.env.URLDB);
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.error('Error de conexión a MongoDB:', err);
+});
+
+db.once('open', () => {
+    console.log('Conectado a MongoDB');
+});
+
+// middlewares
+app.use(express.json());
+app.use("/api", agendaApp);
+app.get('/', function(req, res) { res.json({ mensaje: 'Hola bienvenido esta es la Api-Rest!' }) });
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto: ${port}`);
+});
